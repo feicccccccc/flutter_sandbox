b9d19962e445ff383d5ec25df39695dddeabe49c
fei <feicheung2016@gmail.com>
2022-12-06 22:54:58 +0800

    A demo project that have infinite random color

---
+++ test_app/lib/main.dart
@@ -0,0 +1,46 @@
+// import Material pacakge.
+// Material is a design stadnard from Google
+// Allow us to use various Widgets
+import "package:flutter/material.dart";
+import "dart:math";
+
+// Flutter start run from here
+void main() {
+  runApp(const MyApp()); // global function runApp() to start the application
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  randomColors() {
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  }
+
+  // Get called when flutter need to rebuild the UI
+  // context let widget know where it is in the widget tree.
+  @override
+  Widget build(BuildContext context) {
+    // Return a template using MaterialApp
+    // https://api.flutter.dev/flutter/material/MaterialApp-class.html
+    return MaterialApp(
+      // https://api.flutter.dev/flutter/material/Scaffold-class.html
+      home: Scaffold(
+        body: ListView.builder(itemBuilder: ((context, index) {
+          // return Center(child: Text("I am $index"));
+          return Center(
+            child: Container(
+              color: randomColors(),
+              width: double.infinity,
+              height: 500,
+              child: Center(child: Text("I am index $index")),
+            ),
+          );
+        })),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Center(child: Text("I am a tilte")),
+        ),
+      ),
+    );
+  }
+}
